70e08d0a1d19f3a5d1b579a731ed1dcea7f60b86
shtzr840329 <shtzr840329@gmail.com>
2015-10-18 23:24:04 +0800

    fix machine controller

--- app/controllers/machines_controller.rb
+++ app/controllers/machines_controller.rb
@@ -1,28 +1,38 @@
 class MachinesController < ApplicationController
   protect_from_forgery :except => :index
-  skip_before_filter :verify_authenticity_token,:only => [:create]
+  skip_before_filter :verify_authenticity_token,:only => [:base_hardware_info, :real_hardware_info]
   respond_to :json
 
-  def create
-    machine = Machine.where(identifier: machine_params["identifier"]).first
+  def base_hardware_info
+    machine = Machine.where(identifier: base_hardware_params["identifier"]).first
+    if machine.nil?
+      render :json => { :status => 'error' }
+      return
+    end
     detail = machine.machine_detail
     if detail.nil?
       detail = MachineDetail.new
       detail.machine = machine
     end
-    detail.cpu_name = machine_params["info"]["cpu"]["name"]
-    detail.cpu_real = machine_params["info"]["cpu"]["real"].to_i
-    detail.cpu_total = machine_params["info"]["cpu"]["total"].to_i
-    detail.mhz = machine_params["info"]["cpu"]["mhz"]
+    info = base_hardware_params["info"]
+    detail.cpu_name = info["cpu"]["name"]
+    detail.cpu_real = info["cpu"]["real"].to_i
+    detail.cpu_total = info["cpu"]["total"].to_i
+    detail.mhz = info["cpu"]["mhz"]
     
-    detail.memory_swap_total = machine_params["info"]["memory"]["swap_total"]
-    detail.memory_total = machine_params["info"]["memory"]["total"]
+    detail.memory_swap_total = info["memory"]["swap_total"]
+    detail.memory_total = info["memory"]["total"]
 
-    detail.network_address = machine_params["info"]["net_work"]["network_address"]
+    detail.network_address = info["net_work"]["network_address"]
     detail.save
 
     machine.operating_status = 1
     machine.save
+    render :text => 'ok'
+  end
+
+  def real_hardware_info
+    $redis.hset "real_hardware_info/#{real_hardware_params['identifier']}", "#{real_hardware_params['datetime']}", "#{real_hardware_params['info']}"
     render :text => 'ok'
   end
   
@@ -33,11 +43,20 @@ class MachinesController < ApplicationController
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def machine_params
-      params.require(:machine).permit(:identifier, info: [ 
+    def base_hardware_params
+      params.require(:machine).permit(:identifier, :datetime, info: [ 
                                                       cpu: [ :name, :mhz, :real, :total], 
                                                       memory: [:swap_total, :total ], 
                                                       net_work: [ :external_address, :network_address ] 
+                                                    ])
+    end
+
+    def real_hardware_params
+      params.require(:machine).permit(:identifier, :datetime, info: [ 
+                                                      cpu: [ :real, :top], 
+                                                      file_system: [:lost_file_system, :percent_used],
+                                                      memory: [:memory_total_bytes, :total ], 
+                                                      net_work: [:rx, :tx]
                                                     ])
     end
 end
