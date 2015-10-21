6776a3c7de5896351a89e1ca140fcdfb5987f1f4
shtzr840329 <shtzr840329@gmail.com>
2015-10-21 15:04:36 +0800

    fix log

--- app/decorators/controllers/qy_wechat/qy_wechat_controller_decorator.rb
+++ app/decorators/controllers/qy_wechat/qy_wechat_controller_decorator.rb
@@ -9,27 +9,27 @@ QyWechat::QyWechatController.class_eval do
 
     # text消息
     def response_text_message(options={})
-      generate_text_message("Your Message: #{@keyword}")
+      # generate_text_message("Your Message: #{@keyword}")
     end
 
     # image消息
     def response_image_message(options={})
-      generate_image_message(new_image(@weixin_message.MediaId))
+      # generate_image_message(new_image(@weixin_message.MediaId))
     end
 
     # voice消息
     def response_voice_message(options={})
-      generate_voice_message(new_voice(@weixin_message.MediaId))
+      # generate_voice_message(new_voice(@weixin_message.MediaId))
     end
 
     # video消息
     def response_video_message(options={})
-      generate_video_message(new_video(@weixin_message.MediaId, "desc", "title"))
+      # generate_video_message(new_video(@weixin_message.MediaId, "desc", "title"))
     end
 
     # 用于响应事件
     def response_event_message(options={})
-      generate_text_message("响应事件")
+      # generate_text_message("响应事件")
     end
 
 end
