362e0920ae70231664240b7244163ae61261830d
OnlyCZY <853852998@qq.com>
2015-10-23 14:30:24 +0800

    complete interface

--- app/controllers/admin/interfaces_controller.rb
+++ app/controllers/admin/interfaces_controller.rb
@@ -1,12 +1,61 @@
 module Admin
 	class InterfacesController < BaseController
+		before_action :set_interface, only: [:show, :edit, :update, :destroy]
+		before_action :set_customer, only: [:index, :new, :create, :edit]
+		respond_to :html, :js
 
 		def index
-			
+		end
+
+		def new
+			@interface = @customer.interfaces.new
+			respond_with @interface
+		end
+
+		def create
+			@interface = @customer.interfaces.new(interface_params)
+			if @interface.save
+				flash.now[:notice] = "客户接口创建成功"
+				respond_with @interface
+			else
+				flash.now[:notice] = "客户接口创建失败"
+				render :new
+			end
+		end
+
+		def edit
+			respond_with @interface
+		end
+
+		def update
+			if @interface.update(interface_params)
+				flash.now[:notice] = "客户接口更新成功"
+				respond_with @interface
+			else
+				flash.now[:notice] = "客户接口更新失败"
+				render :edit
+			end
 		end
 
 		def show
-			
 		end
+
+		def destroy
+			@interface.destroy
+      			redirect_to admin_customer_interfaces_path(@interface.customer_id)
+		end
+
+		private
+			def set_interface
+				@interface = Interface.find(params[:id])
+			end
+
+			def set_customer
+				@customer = Customer.find(params[:customer_id])
+			end
+
+			def interface_params
+				params.require(:interface).permit(:name, :identifier)
+			end
 	end
 end
--- app/models/interface.rb
+++ app/models/interface.rb
@@ -1,3 +1,10 @@
 class Interface < ActiveRecord::Base
 	belongs_to :customer
+
+	after_initialize :generate_identifier
+
+	def generate_identifier
+		chars = ("a".."z").to_a + ("A".."Z").to_a + ("0".."9").to_a
+		self.identifier ||= chars.sample(8).join
+	end  
 end
