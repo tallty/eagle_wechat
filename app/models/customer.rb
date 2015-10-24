65a8a0a3de846ce2096b8812ad3e007f7fcc5f80
yuzixun <yzxun@qq.com>
2015-10-24 23:18:03 +0800

    add send info function

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -16,5 +16,6 @@ class Customer < ActiveRecord::Base
   has_many :members, dependent: :destroy
   has_many :interfaces, dependent: :destroy
   has_many :tasks, dependent: :destroy
+  has_many :sms_logs, dependent: :destroy
   validates :name, presence: true
 end
--- app/models/sms_log.rb
+++ app/models/sms_log.rb
@@ -1,3 +1,38 @@
+# == Schema Information
+#
+# Table name: sms_logs
+#
+#  id          :integer          not null, primary key
+#  content     :string(255)
+#  customer_id :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class SmsLog < ActiveRecord::Base
   belongs_to :customer
+
+  def self.send_task_notification task
+    members = task.try(:customer).try(:members)
+    sended_users = members.group(:openid).pluck(:openid)
+
+    text_message = "[上海气象局科科技服务中心]#{name}."
+    task.customer.create(content: text_message)
+    $group_client.message.send_text(sended_users, nil, nil, 1, text_message)
+  end
+
+  def self.check_task
+    $redis.hgetall("task_log_cache").keys.each do |key|
+      identifier, time = key.split("_")
+      
+      task = Task.find_by(identifier: identifier)
+      next unless task.present?
+
+      # 上一次上报的时间
+      now_time = DateTime.now.to_i
+      if (now_time - time.to_i)/60 > task.rate
+        send_task_notification task
+      end
+    end
+  end
 end
