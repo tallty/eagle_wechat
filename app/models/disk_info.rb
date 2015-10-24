a0e360b20033b90ff7fcdde636297bd9ac72ee25
yuzixun <yzxun@qq.com>
2015-10-24 17:35:08 +0800

    add annotate for model

--- app/models/disk_info.rb
+++ app/models/disk_info.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: disk_infos
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  full_name  :string(255)
+#  disk_size  :integer
+#  machine_id :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class DiskInfo < ActiveRecord::Base
   belongs_to :machine
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id          :integer          not null, primary key
+#  identifier  :string(255)
+#  name        :string(255)
+#  rate        :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  customer_id :integer
+#
+
 class Task < ActiveRecord::Base
 	belongs_to :customer
 	after_initialize :task_identifier
