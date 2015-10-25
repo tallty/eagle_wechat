0bac3723839350ff859ce99b69e1c9036823f6b9
OnlyCZY <853852998@qq.com>
2015-10-25 23:26:10 +0800

    add data to reports

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,9 +1,12 @@
 class ReportsController < ApplicationController
 	def index
-		
+		#2015-10-24 接口的记录
+		@reports = $redis.hvals("interface_reports_cache_2015-10-24")
+		#2015-10-24 接口调用总数
+		@total_count = $redis.hget("interface_sum_cache", "X548EYTO_2015-10-24")
 	end
 
 	def show
-		
+
 	end
 end
