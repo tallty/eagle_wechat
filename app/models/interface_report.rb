f5fb3a468e1914ca9ab325634d8e63583fdb409a
shtzr840329 <shtzr840329@gmail.com>
2015-10-25 21:00:23 +0800

    add scope to total_interface

--- app/models/interface_report.rb
+++ app/models/interface_report.rb
@@ -2,6 +2,9 @@ class InterfaceReport < ActiveRecord::Base
 
   def self.process
     now_date = Time.now.to_date - 1.day
-    
+    list = TotalInterface.today(now_date)
+    list.each do |item|
+      p item
+    end
   end
 end
--- app/models/total_interface.rb
+++ app/models/total_interface.rb
@@ -14,6 +14,8 @@
 class TotalInterface < ActiveRecord::Base
   by_star_field :datetime
 
+  scope :today, -> (datetime) { TotalInterface.by_day(datetime).group(:name).sum(:count) }
+  
   def self.process
     
   end
