6f8e90eb087620691899b746af710fd841df77cd
shtzr840329 <shtzr840329@gmail.com>
2015-10-25 21:44:31 +0800

    fix

--- app/models/interface_report.rb
+++ app/models/interface_report.rb
@@ -21,11 +21,19 @@ class InterfaceReport < ActiveRecord::Base
 
   def self.process
     now_date = Time.now.to_date - 1.day
-    list = TotalInterface.day(now_date)
+    list = TotalInterface.by_day(now_date).distinct(:name).pluck(:name)
     list.each do |item|
-      report = InterfaceReport.find_or_create_by datetime: now_date, identifier: 'X548EYTO', name: item[0]
-      report.sum_count = item[1].to_i
-      report
+      data = InterfaceReport.find_or_create_by datetime: now_date, identifier: 'X548EYTO', name: item
+      data.sum_count = TotalInterface.by_day(now_date).where(name: item).sum(:count)
+      items = TotalInterface.by_day(Time.now.to_date - 1.day).where(name: item).order("count asc").first(3)
+      data.first_times = items[0].datetime
+      data.first_count = items[0].count
+      data.second_times = items[1].datetime
+      data.second_count = items[1].count
+      data.third_times = items[2].datetime
+      data.third_count = items[2].count
+      data.save
+      $redis.hset "interface_reports_cache", "#{now_date.strftime('%Y-%m-%d')}_#{data.name}", data.to_json
     end
   end
 end
