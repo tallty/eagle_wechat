04eae753820cb5fb86ca95156a8c1fda7e3c123f
shtzr840329 <shtzr840329@gmail.com>
2015-10-26 06:58:58 +0800

    fix

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,7 +2,7 @@ class ReportsController < ApplicationController
 	def index
 		#2015-10-24 接口的记录
 		cache = $redis.hvals("interface_reports_cache_2015-10-24")
-            @reports = MultiJson.load cache
+            @reports = cache.map { |e| MultiJson.load(e) }
 		#2015-10-24 接口调用总数
 		@total_count = $redis.hget("interface_sum_cache", "X548EYTO_2015-10-24")
 	end
