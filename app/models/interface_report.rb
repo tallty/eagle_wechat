e2a5d84152749957ae0a62bf63e411609f2909b0
shtzr840329 <shtzr840329@gmail.com>
2015-10-25 21:09:53 +0800

    fix

--- app/models/interface_report.rb
+++ app/models/interface_report.rb
@@ -20,7 +20,7 @@ class InterfaceReport < ActiveRecord::Base
 
   def self.process
     now_date = Time.now.to_date - 1.day
-    list = TotalInterface.today(now_date)
+    list = TotalInterface.day(now_date)
     list.each do |item|
       p item
     end
