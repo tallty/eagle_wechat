3787ec10d129f06ffcadb640db9927ddc69f9721
shtzr840329 <shtzr840329@gmail.com>
2015-10-25 21:58:47 +0800

    add sum interface count

--- app/models/interface_report.rb
+++ app/models/interface_report.rb
@@ -36,10 +36,11 @@ class InterfaceReport < ActiveRecord::Base
   def self.process
     now_date = Time.now.to_date - 1.day
     list = TotalInterface.by_day(now_date).distinct(:name).pluck(:name)
+    identifier = "X548EYTO"
     list.each do |item|
-      data = InterfaceReport.find_or_create_by datetime: now_date, identifier: 'X548EYTO', name: item
+      data = InterfaceReport.find_or_create_by datetime: now_date, identifier: identifier, name: item
       data.sum_count = TotalInterface.by_day(now_date).where(name: item).sum(:count)
-      items = TotalInterface.by_day(Time.now.to_date - 1.day).where(name: item).order("count asc").first(3)
+      items = TotalInterface.by_day(now_date).where(name: item).order("count asc").first(3)
       data.first_times = items[0].datetime
       data.first_count = items[0].count
       data.second_times = items[1].datetime
@@ -49,5 +50,7 @@ class InterfaceReport < ActiveRecord::Base
       data.save
       $redis.hset "interface_reports_cache_#{now_date.strftime('%Y-%m-%d')}", "#{data.name}", data.to_json
     end
+
+    $redis.hset "interface_sum_cache", "#{identifier}_#{now_date.strftime('%Y-%m-%d')}", TotalInterface.by_day(now_date).sum(:count)
   end
 end
