b9a941ee346937479925f28c3d5db95829be42bc
OnlyCZY <853852998@qq.com>
2015-10-25 23:36:46 +0800

    fix cpu percent

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -23,7 +23,7 @@ class Machine < ActiveRecord::Base
 
   #计算机器的cpu占比
   def cpu_percent
-      cpu_used = eval(MachineInfo.get_info("cpu", identifier))["cpu_used"].to_f.round(1) 
+      cpu_used = eval(MachineInfo.get_info("cpu", identifier))["cpu_used"].to_i
       "#{cpu_used}%"
   end
 
