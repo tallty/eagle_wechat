6816b6881e31bc8d930c56fdf98060ec02acea0b
shtzr840329 <shtzr840329@gmail.com>
2015-10-26 21:58:59 +0800

    fix interface name

--- app/controllers/total_interfaces_controller.rb
+++ app/controllers/total_interfaces_controller.rb
@@ -5,10 +5,11 @@ class TotalInterfacesController < ApplicationController
   def fetch
     identifier = interface_total_params["identifier"]
     datas = MultiJson.load interface_total_params["data"]
-    p datas
+    
     total_interface = nil
     datas.each do |item|
-      p item
+      name = Interface.where(identifier: item["interface_name"]).first.name
+      next if name.blank?
       total_interface = TotalInterface.find_or_create_by datetime: Time.parse(item["datetime"]), identifier: identifier, name: item["interface_name"]
       total_interface.count = item["interface_count"].to_i
       total_interface.save
