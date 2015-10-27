1fe6d944f379978d327c326d56fe8253c2619424
shtzr840329 <shtzr840329@gmail.com>
2015-10-27 21:37:58 +0800

    fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,11 +11,8 @@ module ApplicationHelper
   end
 
   def time_mini(start_time, end_time)
-<<<<<<< HEAD
     (end_time.to_f - start_time.to_f).round(1)
-=======
-    (end_time.to_i - start_time.to_i)/6000
->>>>>>> 7656fa29784d0963351dd9c8eac4cf83d006697a
+    # (end_time.to_i - start_time.to_i)/6000
   end
   
 end
