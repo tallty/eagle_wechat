0d40efe0f43a31c9905ed69e87163c18808e4c31
zhangliugang <www.zhangyi508@gmail.com>
2015-10-27 21:50:05 +0800

    add db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-
 ActiveRecord::Schema.define(version: 20151027144717) do
 
   create_table "customers", force: :cascade do |t|
