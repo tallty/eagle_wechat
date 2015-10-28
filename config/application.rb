48189dc80970cffc3023f8633325e89544fdc04f
shtzr840329 <shtzr840329@gmail.com>
2015-10-28 09:23:26 +0800

    fix timezone

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,9 @@ module EagleWechat
     # config.i18n.default_locale = :de
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     config.i18n.default_locale = "zh-CN"
+
+    config.time_zone = 'Beijing'
+    config.active_record.default_timezone = :local
     
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
