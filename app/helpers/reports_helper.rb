88cdd1230dded4af89bc2b438387038500edf6e5
OnlyCZY <853852998@qq.com>
2015-10-29 23:57:07 +0800

    fix error

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -8,12 +8,12 @@ module ReportsHelper
 		(active_day + 1).to_time.to_i * 1000
 	end
 
-	def last_month active_month
-		(active_month.beginning_of_month - 1).beginning_of_month.to_time.to_i * 1000
+	def last_month begin_month
+		(active_month - 1).beginning_of_month.to_time.to_i * 1000
 	end
 
-	def next_month active_month
-		(active_month.end_of_month + 1).to_time.to_i * 1000
+	def next_month end_month
+		(end_month + 1).to_time.to_i * 1000
 	end
 
 	def last_week monday
