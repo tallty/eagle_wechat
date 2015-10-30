5aaf13960116c1e14686fe6c64151e25ab7a39d5
OnlyCZY <853852998@qq.com>
2015-10-30 14:40:31 +0800

    test

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,16 +4,18 @@ class ReportsController < ApplicationController
 	before_action :select_month, only: [:month, :month_show]
 	#日报表
 	def index
-		@active_day = params[:date].blank? ? Time.now.to_date : Time.at(params[:date].to_i / 1000).to_date
+		@active_day = params[:date].blank? ? (Time.now.to_date - 1) : Time.at(params[:date].to_i / 1000).to_date
 		cache = $redis.hvals("interface_reports_cache_#{@active_day.strftime("%F")}")
 		@interface_infos = cache.map{ |x| MultiJson.load(x) }
+		#@interface_infos = [{"datetime"=>"2015-10-29", "identifier"=>"X548EYTO", "name"=>"健康气象", "sum_count"=>204, "first_times"=>"2015-10-29 22:00:00 +0800", "first_count"=>12, "second_times"=>"2015-10-29 07:00:00 +0800", "second_count"=>12, "third_times"=>"2015-10-29 23:00:00 +0800", "third_count"=>12}]
 	end
 
 	#日报表详细页
 	def show
-		@active_day = params[:date].blank? ? Time.now.to_date : Time.at(params[:date].to_i / 1000).to_date
+		@active_day = params[:date].blank? ? (Time.now.to_date - 1) : Time.at(params[:date].to_i / 1000).to_date
 		# {user_name => count, ...}
 		@user_infos = {}
+		#@user_infos = {"防雷中心"=>204, "上海腾讯大申网"=>nil, "上海市民信箱信息服务有限公司"=>nil, "青浦气象局APP"=>nil, "上海天气(内部版)"=>nil, "建筑气象服务项目"=>nil, "效益评估项目"=>nil, "上海发布"=>nil, "马拉松"=>nil, "小i智能语音交互平台"=>nil, "上海交通大学"=>nil, "交通委"=>nil}
 		Customer.first.api_users.each do |user|
 			@user_infos["#{user.company}"] = user.total_interfaces.day(@active_day).where(name: params[:name]).sum(:count)[params[:name]]
 		end
