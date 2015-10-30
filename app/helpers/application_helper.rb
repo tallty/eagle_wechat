d4eba1011603cf98bf9a3cccf0376cfca6e3ad6b
zhangliugang <www.zhangyi508@gmail.com>
2015-10-30 14:44:53 +0800

    add port time delay

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,5 +15,12 @@ module ApplicationHelper
     pre_tl = task_logs.select{ |tl| tl.start_time < current_task_log.start_time && tl.task_identifier == current_task_log.task_identifier }.first
     pre_tl.present? ?  current_task_log.start_time - pre_tl.start_time : 0
   end
+
+  def delay( len )
+    chars = ("100".."200").to_a
+    newpass = ""
+    1.upto(len) { |i| newpass << chars[rand(chars.size-1)] }
+    return newpass
+  end
   
 end
