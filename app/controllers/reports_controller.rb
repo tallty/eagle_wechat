8632257661298527682212d3febb99bbc75656e2
OnlyCZY <853852998@qq.com>
2015-11-05 01:00:56 +0800

    fix

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -26,57 +26,29 @@ class ReportsController < ApplicationController
 	#日报表详细页
 	def show
 		# 借口的图表数据
-		@interface_info = TotalInterface.api_user_infos(current_customer, params[:name], @active_day)[:interface]
+		@interface_info = TotalInterface.interface_info(current_customer, params[:name], @active_day, :day)
 		# 调用已选借口的所有客户信息
-		@api_user_infos = TotalInterface.api_user_infos(current_customer, params[:name], @active_day)[:api_users]
+		@api_user_infos = TotalInterface.api_user_infos(current_customer, params[:name], @active_day, :day)
 		# 调用总数
 		@total_count = TotalInterface.total_count(@api_user_infos)
 	end
 
 	#周报表详情页
 	def week_show
-		#@user_infos = TotalInterface.between_times(@monday, @sunday).includes(:api_user).where(name: params[:name]).group(:company).order(:count).sum(:count)
-		
-		total_interfaces = current_customer.total_interfaces.week(@monday)
-		interface = Interface.where(name: params[:name]).first
-		# 选中接口的调用信息，取 :every_count 用于显示图表
-		@interface_info = interface.by_day_infos(total_interfaces)
-
-		@api_user_infos = {}
-		current_customer.api_users.each do |api_user|
-			total_interfaces = api_user.total_interfaces.week(@monday)
-			api_user_info = interface.by_day_infos(total_interfaces)
-			unless api_user_info[:sum_count] == 0
-				@api_user_infos[api_user.company] = api_user_info
-			end
-		end
-		# 循环显示所有调用选中接口的客户的调用信息
-		@api_user_infos = @api_user_infos.sort{ |x,y| y[1][:sum_count] <=> x[1][:sum_count] }.to_h
-
+		# 借口的图表数据
+		@interface_info = TotalInterface.interface_info(current_customer, params[:name], @monday, :week)
+		# 调用已选借口的所有客户信息
+		@api_user_infos = TotalInterface.api_user_infos(current_customer, params[:name], @monday, :week)
 		# 调用总数
 		@total_count = TotalInterface.total_count(@api_user_infos)
 	end
 
 	#月报表详情页
 	def month_show
-		#@user_infos = TotalInterface.between_times(@begin_month, @end_month).includes(:api_user).where(name: params[:name]).group(:company).order(:count).sum(:count)
-	
-		total_interfaces = current_customer.total_interfaces.month(@begin_month)
-		interface = Interface.where(name: params[:name]).first
-		# 选中接口的调用信息，取 :every_count 用于显示图表
-		@interface_info = interface.by_day_infos(total_interfaces)
-
-		@api_user_infos = {}
-		current_customer.api_users.each do |api_user|
-			total_interfaces = api_user.total_interfaces.month(@begin_month)
-			api_user_info = interface.by_day_infos(total_interfaces)
-			unless api_user_info[:sum_count] == 0
-				@api_user_infos[api_user.company] = api_user_info
-			end
-		end
-		# 循环显示所有调用选中接口的客户的调用信息
-		@api_user_infos = @api_user_infos.sort{ |x,y| y[1][:sum_count] <=> x[1][:sum_count] }.to_h
-
+	# 借口的图表数据
+		@interface_info = TotalInterface.interface_info(current_customer, params[:name], @monday, :month)
+		# 调用已选借口的所有客户信息
+		@api_user_infos = TotalInterface.api_user_infos(current_customer, params[:name], @monday, :month)
 		# 调用总数
 		@total_count = TotalInterface.total_count(@api_user_infos)
 	end
