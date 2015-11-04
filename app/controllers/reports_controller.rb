1e512b93351030cd318d87c08d577e729151f6ff
OnlyCZY <853852998@qq.com>
2015-11-05 01:07:38 +0800

    fix

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -46,9 +46,9 @@ class ReportsController < ApplicationController
 	#月报表详情页
 	def month_show
 	# 借口的图表数据
-		@interface_info = TotalInterface.interface_info(current_customer, params[:name], @monday, :month)
+		@interface_info = TotalInterface.interface_info(current_customer, params[:name], @begin_month, :month)
 		# 调用已选借口的所有客户信息
-		@api_user_infos = TotalInterface.api_user_infos(current_customer, params[:name], @monday, :month)
+		@api_user_infos = TotalInterface.api_user_infos(current_customer, params[:name], @begin_month, :month)
 		# 调用总数
 		@total_count = TotalInterface.total_count(@api_user_infos)
 	end
