ec1bb423f38af9271f41e925299f0def7181a42a
OnlyCZY <853852998@qq.com>
2015-11-16 12:15:01 +0800

    add admin/api_users

--- app/controllers/admin/api_users_controller.rb
+++ app/controllers/admin/api_users_controller.rb
@@ -1,12 +1,14 @@
 module Admin
 	class ApiUsersController < BaseController
+		respond_to :html, :js
+
 		def index
 			@interface = Interface.find(params[:interface_id])
 			@api_users = interface.api_users
 		end
 
 		def new
-
+			
 		end
 
 		def create
