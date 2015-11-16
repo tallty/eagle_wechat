9523819e4b569bcd6716e3e04dc26cbcc62bb319
zhangliugang <www.zhangyi508@gmail.com>
2015-11-16 17:16:12 +0800

    change date time

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -31,6 +31,6 @@ module ApplicationHelper
   end
 
   def get_task_time task_log
-    log_time = TaskLog.order(end_time: :DESC).where({task_identifier: task_log.task_identifier}).first.end_time
+    log_time = TaskLog.order(end_time: :DESC).where({task_identifier: task_log.task_identifier}).first.start_time
   end
 end
