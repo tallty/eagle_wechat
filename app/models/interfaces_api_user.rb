281e03ce12b4914161f56068805131fa4799026a
OnlyCZY <853852998@qq.com>
2015-11-16 14:13:37 +0800

    fix

--- app/models/interfaces_api_user.rb
+++ app/models/interfaces_api_user.rb
@@ -1,4 +1,15 @@
 class InterfacesApiUser < ActiveRecord::Base
   belongs_to :interface
   belongs_to :api_user
+
+   def self.save_interface_api_users(interface_id, api_users)
+    create_api_users = api_users.to_a
+    InterfacesApiUser.transaction do
+      unless create_api_users.empty?
+        create_api_users.each do |api_user_id|
+          InterfacesApiUser.create(interface_id: interface_id.to_i, api_user_id: api_user_id)
+        end
+      end
+    end
+  end
 end
