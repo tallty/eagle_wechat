41e69e5cc781f0f8c2eca22b6955b488611db0d5
OnlyCZY <853852998@qq.com>
2015-11-16 15:05:28 +0800

    fix

--- app/controllers/admin/api_users_controller.rb
+++ app/controllers/admin/api_users_controller.rb
@@ -15,7 +15,7 @@ module Admin
 
 		def create
 			InterfacesApiUser.save_interface_api_users(params[:interface_id], params[:interface_api_users])
-			return redirect_to admin_interface_api_users_path(interface_id: params[:interface_id], customer_id: params[:customer])
+			return redirect_to admin_interface_api_users_path(interface_id: params[:interface_id], customer_id: params[:customer_id])
 		end
 
 		def destroy
