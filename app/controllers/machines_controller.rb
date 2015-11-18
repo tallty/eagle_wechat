801ae0b2660d2e3b0b19a9bca8d80d9a8dfd6185
shtzr840329 <shtzr840329@gmail.com>
2015-11-18 17:41:15 +0800

    add fetch data log time

--- app/controllers/machines_controller.rb
+++ app/controllers/machines_controller.rb
@@ -36,6 +36,7 @@ class MachinesController < ApplicationController
     $redis.lpush "#{real_hardware_params['identifier']}_memory", "#{real_hardware_params['info']['memory']}"
     $redis.lpush "#{real_hardware_params['identifier']}_net_work", "#{real_hardware_params['info']['net_work']}"
     $redis.lpush "#{real_hardware_params['identifier']}_file_systems", "#{real_hardware_params['info']['file_system']}"
+    $redis.hset("machine_last_update_time", "#{real_hardware_params['identifier']}", Time.now.strftime('%Y-%m-%d %H:%M:%S'))
     render :text => 'ok'
   end
 
@@ -59,7 +60,8 @@ class MachinesController < ApplicationController
                                                       cpu: [ :real, :top, :cpu_used, :date_time ], 
                                                       file_system: [:lost_file_system, :percent_used, :date_time],
                                                       memory: [:memory_total_bytes, :total, :memory_free_bytes, :memory_inactive_bytes, :memory_wired_bytes, :date_time ], 
-                                                      net_work: [:rx, :tx, :date_time]
+                                                      net_work: [:rx, :tx, :date_time],
+                                                      load_average: [:date_time, :load_fifteen_minutes, :load_five_minutes, :load_one_minute]
                                                     ])
     end
 end
