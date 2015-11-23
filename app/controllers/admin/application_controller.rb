ff0b826b759160031b2445f5ce9f60d8fe317b08
shtzr840329 <shtzr840329@gmail.com>
2015-11-23 16:36:48 +0800

    add filter

---
+++ app/controllers/admin/application_controller.rb
@@ -0,0 +1,12 @@
+module Admin
+  class ApplicationController < ActionController::Base
+    # Prevent CSRF attacks by raising an exception.
+    # For APIs, you may want to use :null_session instead.
+    protect_from_forgery with: :exception
+    skip_before_filter :verify_authenticity_token
+    before_filter :authenticate_user!
+    layout 'admin/home'
+    
+  end
+
+end
