56ca46d2887cc5c87e024be3403af80334508dd4
shtzr840329 <shtzr840329@gmail.com>
2015-12-06 01:07:05 +0800

    close send message

--- app/services/recent_tasks.rb
+++ app/services/recent_tasks.rb
@@ -1,6 +1,6 @@
 class RecentTasks
 
   def self.push(raw_post)
-    $group_client.message.send_text("alex6756", "", "", 1, raw_post)
+    # $group_client.message.send_text("alex6756", "", "", 1, raw_post)
   end
 end
