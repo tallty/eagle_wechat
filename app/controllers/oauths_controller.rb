d70dd44f3dd3b660df6fa62cd655d828aedc9dd2
shtzr840329 <shtzr840329@gmail.com>
2015-12-07 22:03:37 +0800

    fix

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -2,26 +2,29 @@ class OauthsController < ApplicationController
   # before_action :store_reurl, only: [:index]
 
   respond_to :html
-  
+
   def index
-    # openid = session[:openid]
-    # if openid.present?
-    target_url = "\/#{params[:target_url]}"
-    redirect_to target_url
-    # else
-    #   url = $group_client.oauth.authorize_url("http://mcu.buoyantec.com/#{params['target_url']}", "STATE#wechat_redirect")
-    #   uri = URI.encode(url)
-    #   redirect_to uri
-    # end
+    p '-------------------openid in oauths controller----------------------'
+    openid = session[:openid]
+    p "openid param is: #{openid}"
+
+    if openid.present?
+      target_url = "\/#{params[:target_url]}"
+      redirect_to target_url
+    else
+      url = $group_client.oauth.authorize_url("http://mcu.buoyantec.com/#{params['target_url']}", "STATE#wechat_redirect")
+      uri = URI.encode(url)
+      redirect_to uri
+    end
   end
 
   def check
   end
 
   def current_customer
-    
+
   end
-  
+
   private
   def store_reurl
     # session[:recurl] += "#{(session[:recurl].include? '?') ? '&' : '?'}body=#{URI.escape(params[:body])}" if params[:body].present?
