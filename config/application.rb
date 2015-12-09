33b114a44cf829822195105a26448968149bab95
shtzr840329 <shtzr840329@gmail.com>
2015-12-10 01:36:36 +0800

    fix

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module EagleWechat
       end
     end
 
+    config.autoload_paths += %W(#{Rails.root}/app/workers)
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -30,7 +32,7 @@ module EagleWechat
     # config.i18n.default_locale = :de
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     config.i18n.default_locale = "zh-CN"
-    
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
