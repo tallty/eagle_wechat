30d85b29eeb0347c26ca2f863d2d3cbe4f3073f2
shtzr840329 <shtzr840329@gmail.com>
2015-12-09 14:33:32 +0800

    fix

--- app/services/recent_tasks.rb
+++ app/services/recent_tasks.rb
@@ -1,7 +1,7 @@
 class RecentTasks
 
   def self.push(raw_post)
-    logger.warn raw_post
+    Rails.logger.warn raw_post
     # $group_client.message.send_text("alex6756", "", "", 1, raw_post)
   end
 end
