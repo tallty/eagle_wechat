eb024e6eefce48fee8be6a0aa9bcb86088722b05
shtzr840329 <shtzr840329@gmail.com>
2015-12-13 17:33:15 +0800

    fix

--- app/controllers/api_users_controller.rb
+++ app/controllers/api_users_controller.rb
@@ -6,7 +6,12 @@ class ApiUsersController < ApplicationController
 
   def index
     @day_format = @active_day.strftime("%Y-%m-%d")
-    @api_users = current_customer.api_users
+    @api_users = current_customer.api_users.as_json
+    @count = TotalInterface.user_analyz(@active_day)
+    @api_users.each do |user|
+      user['count'] = @count[user['id']]
+    end
+    @api_users.sort { |a, b| b['count'] <=> a['count'] }
   end
 
   def daily
