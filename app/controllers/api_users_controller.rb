5e01d58a2b50f043da0e1e18788036a1b2b76457
shtzr840329 <shtzr840329@gmail.com>
2015-12-13 17:40:33 +0800

    print log

--- app/controllers/api_users_controller.rb
+++ app/controllers/api_users_controller.rb
@@ -12,7 +12,7 @@ class ApiUsersController < ApplicationController
       user['count'] = @count[user['id']]
     end
     @api_users.sort { |a, b| b['count'] <=> a['count'] }
-    p @api_users
+    logger.warn @api_users
   end
 
   def daily
