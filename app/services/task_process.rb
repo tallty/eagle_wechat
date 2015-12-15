8048ad482ca084037d3c2be031ca7936d51878f2
shtzr840329 <shtzr840329@gmail.com>
2015-12-15 23:51:45 +0800

    fix

--- app/services/task_process.rb
+++ app/services/task_process.rb
@@ -8,13 +8,14 @@ class TaskProcess
     process_result = item["process_result"]
     return if item["task_identifier"].blank?
 
+    file_name = MultiJson.load(process_result['file_list']).join(';') rescue ""
     log_params = {
       task_identifier: item['task_identifier'],
       start_time: Time.at(process_result['start_time'].to_f),
       end_time: Time.at(process_result['end_time'].to_f),
       exception: process_result['exception'],
       task_name: Task.get_task_name(item['task_identifier']),
-      file_name: MultiJson.load(process_result['file_list']).join(';') rescue ""
+      file_name: file_name
     }
     log = TaskLog.new.build_task_log(log_params)
     $redis.hset("alarm_task_cache", log.task_identifier, log.start_time)
