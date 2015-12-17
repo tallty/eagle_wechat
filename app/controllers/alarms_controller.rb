e85501f91f1111b1082b0d0bd2bd01da8ad3430f
shtzr840329 <shtzr840329@gmail.com>
2015-12-17 12:05:13 +0800

    fix

---
+++ app/controllers/alarms_controller.rb
@@ -0,0 +1,30 @@
+class AlarmController < ApplicationController
+  skip_before_filter :verify_authenticity_token, :only => [:index, :week, :month]
+
+	before_action :current_customer
+
+  def index
+    page = params[:page] || params['page'] || 1
+    @alarms = @customer.alarms.paginate(:page => page, :per_page => 10)
+  end
+
+  private
+		def current_customer
+			openid = session[:openid]
+			if openid.blank?
+				code = params[:code]
+				result = $group_client.oauth.get_user_info(code, "1")
+				openid = result.result["UserId"]
+			end
+
+			member = Member.where(openid: openid).first
+			@customer = nil
+			if member.present?
+				session[:openid] = openid
+				@customer = member.customer
+			else
+				@customer = Customer.first
+			end
+			return @customer
+		end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -78,5 +78,7 @@ Rails.application.routes.draw do
        end
      end
    end
+   
+   resources :alarms
 
 end
