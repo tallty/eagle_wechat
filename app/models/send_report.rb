800899c38a34129c6d67f78046996098f80f5445
shtzr840329 <shtzr840329@gmail.com>
2015-12-21 15:45:02 +0800

    fix

--- app/models/send_report.rb
+++ app/models/send_report.rb
@@ -6,9 +6,9 @@ class SendReport
     now_day = Time.now
     today = now_day - 1.day
     if now_day.strftime("%u").eql?("1")
-      articles << {:title => "#{today.strftime('%Y年第%W周监控报表')}", :url => "http://mcu.buoyantec.com/oauths?target_url=reports/week", :picurl => "http://mcu.buoyantec.com/assets/images/week_report.png"}
+      articles << {:title => "#{today.strftime('%Y年第%W周监控报表')}", :url => "http://mcu.buoyantec.com/oauths?target_url=reports/week_index&date=#{today.strftime('%Y-%m-%d')}", :picurl => "http://mcu.buoyantec.com/assets/images/week_report.png"}
     end
-    articles << {:title => "#{today.strftime('%Y年%m月%d日监控报表')}", :url => "http://mcu.buoyantec.com/oauths?target_url=reports&date=#{now_day.strftime('%Y-%m-%d')}", :picurl => "http://mcu.buoyantec.com/assets/images/day_report.png"}
+    articles << {:title => "#{today.strftime('%Y年%m月%d日监控报表')}", :url => "http://mcu.buoyantec.com/oauths?target_url=reports&date=#{today.strftime('%Y-%m-%d')}", :picurl => "http://mcu.buoyantec.com/assets/images/day_report.png"}
     $group_client.message.send_news("", "4", "", 1, articles, safe=0)
   end
 
