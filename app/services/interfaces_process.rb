20790e4928e129ad58860fc8ccc7039b06fc8353
shtzr840329 <shtzr840329@gmail.com>
2016-01-14 10:16:16 +0800

    fix

--- app/services/interfaces_process.rb
+++ app/services/interfaces_process.rb
@@ -1,7 +1,7 @@
 class InterfacesProcess
 
   def self.push(raw_post)
-    logger.warn ">>>>>>>>>>>>>>>>>>接收接口计数,执行任务<<<<<<<<<<<<<<<<<<<<"
+    Rails.logger.warn ">>>>>>>>>>>>>>>>>>接收接口计数,执行任务<<<<<<<<<<<<<<<<<<<<"
     TotalInterface.new.analyz_fetch_data raw_post
   end
 end
