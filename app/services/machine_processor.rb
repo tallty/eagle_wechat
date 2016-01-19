09c7546dce7c40c2a943f8e0fefab5a451511b1d
shtzr840329 <shtzr840329@gmail.com>
2016-01-19 15:08:17 +0800

    fix

--- app/services/machine_processor.rb
+++ app/services/machine_processor.rb
@@ -4,15 +4,17 @@ class MachineProcessor
   def self.push(raw_post)
     real_hardware_params = MultiJson.load raw_post rescue {}
     return if real_hardware_params.blank?
-    $redis.lpush "#{real_hardware_params['identifier']}_cpu", "#{real_hardware_params['info']['cpu']}"
-    $redis.lpush "#{real_hardware_params['identifier']}_memory", "#{real_hardware_params['info']['memory']}"
-    $redis.lpush "#{real_hardware_params['identifier']}_net_work", "#{real_hardware_params['info']['net_work']}"
-    $redis.lpush "#{real_hardware_params['identifier']}_file_systems", "#{real_hardware_params['info']['file_system']}"
-    $redis.hset("machine_last_update_time", "#{real_hardware_params['identifier']}", Time.now.strftime('%Y-%m-%d %H:%M:%S'))
-    $redis.ltrim "#{real_hardware_params['identifier']}_cpu", 0, 8640
-    $redis.ltrim "#{real_hardware_params['identifier']}_memory", 0, 8640
-    $redis.ltrim "#{real_hardware_params['identifier']}_net_work", 0, 8640
-    $redis.ltrim "#{real_hardware_params['identifier']}_file_systems", 0, 8640
+    $redis.multi do
+      $redis.lpush "#{real_hardware_params['identifier']}_cpu", "#{real_hardware_params['info']['cpu']}"
+      $redis.lpush "#{real_hardware_params['identifier']}_memory", "#{real_hardware_params['info']['memory']}"
+      $redis.lpush "#{real_hardware_params['identifier']}_net_work", "#{real_hardware_params['info']['net_work']}"
+      $redis.lpush "#{real_hardware_params['identifier']}_file_systems", "#{real_hardware_params['info']['file_system']}"
+      $redis.hset("machine_last_update_time", "#{real_hardware_params['identifier']}", Time.now.strftime('%Y-%m-%d %H:%M:%S'))
+      $redis.ltrim "#{real_hardware_params['identifier']}_cpu", 0, 8640
+      $redis.ltrim "#{real_hardware_params['identifier']}_memory", 0, 8640
+      $redis.ltrim "#{real_hardware_params['identifier']}_net_work", 0, 8640
+      $redis.ltrim "#{real_hardware_params['identifier']}_file_systems", 0, 8640
+    end
   end
 
 end
