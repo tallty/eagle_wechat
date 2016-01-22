2459d21c8fc4dca9bf6f7654e018dc738b9089ad
shtzr840329 <shtzr840329@gmail.com>
2016-01-22 09:06:49 +0800

    add sneakers config

--- config/initializers/sneakers.rb
+++ config/initializers/sneakers.rb
@@ -1,2 +1,8 @@
-Sneakers.configure({})
+Sneakers.configure({
+    :workers => 2,
+    :threads => 20,
+    :share_threads => true,
+    :log => 'log/sneakers.log',
+    :daemonize => true
+})
 Sneakers.logger.level = Logger::INFO
