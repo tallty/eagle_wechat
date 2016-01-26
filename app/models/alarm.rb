c922d00d175359776cc697a39879d7aba8e1cebd
shtzr840329 <shtzr840329@gmail.com>
2016-01-26 17:35:42 +0800

    fix

--- app/models/alarm.rb
+++ app/models/alarm.rb
@@ -58,7 +58,7 @@ class Alarm < ActiveRecord::Base
         machine_name = machine_hash[:name] || machine_hash['name']
         machine_customer = machine_hash[:customer] || machine_hash['customer']
         # 服务器故障时的采集信息列表长度
-        length = length = $redis.llen("#{e}_cpu").to_i
+        length = $redis.llen("#{e}_cpu").to_i
         params = {
           identifier: e,
           title: machine_name,
@@ -69,6 +69,7 @@ class Alarm < ActiveRecord::Base
           content: "超时未收到数据!!!"
         }
         alarm = Alarm.where(identifier: e, alarmed_at: alarm_time).first
+        p alarm
         # 判断是否存在此告警
         if alarm.present?
           # 判断是否推送此消息
@@ -101,38 +102,6 @@ class Alarm < ActiveRecord::Base
       alarm = Alarm.create(params)
       alarm.send_log.find_or_create_by(accept_user: 'alex6756', info: alarm.content)
     end
-  end
-
-	# 判断是否需要告警，并返回告警记录
-	def self.avtive_alarms(current_customer)
-    cache = {}
-    current_customer.machines.each do |machine|
-      # 找到服务器最新的告警
-      #   如果存在告警，判断告警是否解除
-      #     解除：不存入cache；未解除：存入cache
-      alarm = Alarm.where(identifier: machine.identifier).last
-      if alarm.present?
-        if alarm.warn_over_time.nil?
-          cache["#{machine.name}"] = ["#{alarm.category}", "#{alarm.alarmed_at}"]
-        end
-      end
-    end
-    return cache
-	end
-
-	# 告警解除时间
-  def warn_over_time
-    # 服务器采集信息列表
-  	cache = $redis.lindex("#{identifier}_cpu", -(rindex + 1))
-    # 接口信息列表 TODO
-
-    # 气象数据信息列表 TODO
-
-  	if cache.nil?
-  		return nil
-  	else
-  		return eval(cache)["date_time"].to_time.strftime("%y-%m-%d %H:%M")
-  	end
   end
 
 end
