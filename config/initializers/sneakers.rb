2669c4518f3d1f228d4548aa86a85bed89171f0d
shtzr840329 <shtzr840329@gmail.com>
2016-03-29 11:28:45 +0800

    close sneakers

--- config/initializers/sneakers.rb
+++ config/initializers/sneakers.rb
@@ -1,9 +1,9 @@
-Sneakers.configure({
-    :workers => 2,
-    :threads => 20,
-    :share_threads => true,
-    :log => 'log/sneakers.log',
-    :daemonize => true,
-    :env => 'production'
-})
-Sneakers.logger.level = Logger::INFO
+# Sneakers.configure({
+#     :workers => 2,
+#     :threads => 20,
+#     :share_threads => true,
+#     :log => 'log/sneakers.log',
+#     :daemonize => true,
+#     :env => 'production'
+# })
+# Sneakers.logger.level = Logger::INFO
