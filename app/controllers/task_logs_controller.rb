6d296a7a9e2c3d88161adfa38141e2d16c15feb8
shtzr840329 <shtzr840329@gmail.com>
2016-03-29 11:08:28 +0800

    delete mq in task log

--- app/controllers/task_logs_controller.rb
+++ app/controllers/task_logs_controller.rb
@@ -4,7 +4,21 @@ class TaskLogsController < ApplicationController
   respond_to :json
 
   def fetch
-    BasePublisher.publish("task", task_log_params.to_json)
+    # BasePublisher.publish("task", task_log_params.to_json)
+
+    return if task_log_params["task_identifier"].blank?
+    process_result = item["process_result"]
+    file_name = MultiJson.load(process_result['file_list']).join(';') rescue ""
+    log_params = {
+      task_identifier: task_log_params['task_identifier'],
+      start_time: Time.at(process_result['start_time'].to_f),
+      end_time: Time.at(process_result['end_time'].to_f),
+      exception: process_result['exception'],
+      task_name: Task.get_task_name(task_log_params['task_identifier']),
+      file_name: file_name
+    }
+    log = TaskLog.new.build_task_log(log_params)
+    $redis.hset("alarm_task_cache", log.task_identifier, log.start_time)
     render :text => 'ok'
   end
 
